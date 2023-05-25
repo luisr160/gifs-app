898e8ce3f91bd22701ba02247ff998f46f212789
luisr160 <luis.rosero1@utp.ac.pa>
2023-05-25 14:28:15 -0500

    minor tweak

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GifsService } from 'src/app/gifs/services/gifs.service';
-import { Onclick } from '../../../gifs/interfaces/gifs.interfaces';
 
 @Component({
   selector: 'shared-sidebar',
